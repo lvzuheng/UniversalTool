164670d70c83ed7be8d409b9b07e75663e6794cc
lvzuheng <289019372@qq.com>
2017-12-08 16:20:25 +0800

    修正

---
+++ src/com/lzh/utils/count/math/GDCMain.java
@@ -0,0 +1,10 @@
+package com.lzh.utils.count.math;
+
+public class GDCMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(GreatestCommonDivisor.getGCD(9999, 999999));
+	}
+
+}
--- src/com/lzh/utils/count/math/GreatestCommonDivisor.java
+++ src/com/lzh/utils/count/math/GreatestCommonDivisor.java
@@ -2,27 +2,31 @@ package com.lzh.utils.count.math;
 
 /**
  * @author lzh
- *
- *求最大公约数：更相减损术
+ *存疑
+ *求最大公约数：辗转相除法结合更相减损术
  *
  *两个正整数a和b（a>b），它们的最大公约数等于a-b的差值c和较小数b的最大公约数。比如10和25，25减去10的差是15,那么10和25的最大公约数，等同于10和15的最大公约数。
  */
 public class GreatestCommonDivisor {
 
 	public static int getGCD(int a,int b){
-		int resule = 1;
-		if(a == b)
-			resule =  a;
-		if(a >b)
-			resule =  gcd(a, b);
-		if(a < b)
-			resule =  gcd(b, a);
-		return resule;
+		return gcd(a, b);
 	}
 	
 	private static int gcd(int a,int b){
-		if(a%b == 0)
+		if(a == b)
 			return b;
-		return gcd(b, a%b);
+		if(b>a){
+			return gcd(b, a);
+		}
+		if((a&1) == 0 && (b & 1) ==0){
+			return gcd(a >> 1, b >> 1) << 1;
+		}else if((a&1)==0 && (b&1) ==1){
+			return gcd(a >> 1, b);
+		}else if ((a&1)==1 && (b&1) ==0) {
+			return gcd(a, b>>1);
+		}else {
+			return gcd(a-b, b);
+		}
 	}
 }
